7ee19721c708c60f742c61ab3d201c3ff101d7df
Aminul Asif <aminulasif20@gmail.com>
2024-09-28 17:00:33 +0600

    blog and history part done

---
+++ js/blogs.js
@@ -0,0 +1,6 @@
+document.getElementById("home").addEventListener("click" ,function (){
+      window.location.href="index.html"
+          });
+          document.getElementById("history").addEventListener("click" ,function (){
+            window.location.href="history.html"
+                });
---
+++ js/history.js/history.js
@@ -0,0 +1,4 @@
+document.getElementById("history").addEventListener("click" ,function (){
+      window.location.href="history.html"
+          });
+         
